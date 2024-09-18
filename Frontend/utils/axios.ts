cb9deaaa55847f9b146eed6f23e7c308539f3a22
Kaayn <leo.dimartinopro@gmail.com>
2024-09-18 15:33:01 +0200

    pragma delete

--- Frontend/utils/axios.ts
+++ Frontend/utils/axios.ts
@@ -4,16 +4,13 @@ import axios from "axios";
 const API_PROD_URL = "https://api.sign-e.fr/";
 const API_DEV_URL = "http://localhost:3000/";
 const baseURL = "http://localhost:3001/";
-// TODO: Mettre jobbi.com
+// TODO: Mettre jobbi.fr
 
 const axiosClient = () =>
   axios.create({
     baseURL,
     headers: {
       Authorization: $accessToken.get(),
-      "Cache-Control": "no-cache",
-      Pragma: "no-cache",
-      Expires: "0",
     },
   });
 
